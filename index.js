a4a920981ee6c06f83faed40ad540bb0b1fb4e5c
rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-12 07:09:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  const kittens = ["Milo", "Otis", "Garfield"]
-  const kittens1 = ["lola"]
+  const kittens = ["Milo", "Otis", "Garfield"];
+  const destructivelyAppendKitten = ["lola"];
+
+  kittens.const(destructivelyAppendKitten);
 }
